7f1ff791a94d8988b726a691b99374360642f8e2
Deshon Morgan <deshonmorgan1317@gmail.com>
2024-04-15 22:53:17 +0000

    building frontend skeleton and api connections

---
+++ frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Logout() {
+  // clears tokens
+  localStorage.clear();
+
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  // clear so that we dont return old access tokens, prevents functionality errors
+  localStorage.clear();
+
+  return <Register />;
+}
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              {/* cant access home unless user is authenticated */}
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/logout"
+          element={<Logout />}
+        />
+        <Route
+          path="/register"
+          element={<RegisterAndLogout />}
+        />
+        <Route
+          path="/*"
+          // 404 page
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
